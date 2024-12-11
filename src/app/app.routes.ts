9517f3db514de4c90d078b915152f49184dbc9c4
Tom Xaviour <tomxaviour23@gmail.com>
2024-12-11 14:08:13 -0500

    Setup frontend routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { TeamComponent } from './team/team.component';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TeamComponent },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
