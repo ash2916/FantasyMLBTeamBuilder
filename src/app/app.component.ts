d4eb51e3490ca96a1000b4387cb7ee08d95725b4
Tom Xaviour <tomxaviour23@gmail.com>
2024-12-11 14:07:52 -0500

    Setup Global App Structure & styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [RouterModule, MatToolbarModule],
 })
 export class AppComponent {
-  title = 'baseball-team-builder';
+  title = 'Fantasy MLB';
 }
