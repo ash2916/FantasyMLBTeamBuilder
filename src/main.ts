228714b7c6b14d4df932ceb5ad414d3aec324e4c
Tom Xaviour <tomxaviour23@gmail.com>
2024-12-11 14:12:23 -0500

    Refactored UI folder

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app.routes';
-import { provideRouter } from '@angular/router';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
-}).catch(err => console.error(err));
