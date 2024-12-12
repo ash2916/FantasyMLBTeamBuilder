d9891cf73489a5b2c6f49182ba469eb82786ffac
Tom Xaviour <tomxaviour23@gmail.com>
2024-12-12 00:00:39 -0500

    New branch for netlify

--- backend/mlb/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TeamListView, TeamPlayersView, default_players
-
-urlpatterns = [
-    path('api/default-players/', default_players, name='default-players'),
-    path('api/teams/', TeamListView.as_view(), name='team-list'),
-    path('api/teams/<int:team_id>/players/', TeamPlayersView.as_view(), name='team-players'),
-]
